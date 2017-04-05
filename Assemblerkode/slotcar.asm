bdef178f2ee6495f8bd799586ab2af6e3559dfa6
kristianklein <kristian_jacobsen@hotmail.com>
2017-04-05 18:19:52 +0200

    Ryddet op og rettet interrupts (Andreas havde ret!)

--- Assemblerkode/slotcar.asm
+++ Assemblerkode/slotcar.asm
@@ -1,5 +1,5 @@
 .INCLUDE "m32Adef.inc"
-.EQU F_CPU = 16000000
+.EQU F_CPU = 1000000
 
 
 ;-------------------;
@@ -40,24 +40,14 @@
 	OUT SPH, @0
 .ENDMACRO
 
-.MACRO MOTOR_SPEED
-	LDI @0, @1
-	OUT OCR2, @0
-.ENDMACRO
-
-.MACRO READ_ADC
-	IN @0, ADCL
-	IN @1, ADCH
-.ENDMACRO
-
 ;-------------------;
 ;   VECTOR TABLE    ;
 ;-------------------;
 .ORG 0x00
 RJMP setup ; Reset
 
-;.ORG 0x02
-;RJMP distance_interrupt ; Interrupt on INT0 (PD2)
+.ORG 0x02
+RJMP distance_interrupt ; Interrupt on INT0 (PD2)
 
 .ORG 0x04
 RJMP finish_line_interrupt ; Intterupt on INT1 (PD3)
@@ -67,7 +57,7 @@ RJMP finish_line_interrupt ; Intterupt on INT1 (PD3)
 ;-------------------;
 .ORG 0x2A
 setup:
-	SSP R16, RAMEND
+	SSP R16, RAMEND ; Set stack pointer
 	
 	; Set RED_LED as output and low
 	SBI RED_LED_DDR, RED_LED
@@ -104,15 +94,16 @@ setup:
 	LDI R16, 0b01101100
 	OUT TCCR2, R16
 	
-	; Set MOTOR_SPEED to 90 (duty cycle = 90/256 = 35%)
-	MOTOR_SPEED R16, 90
+	; Set motor speed to 90 (duty cycle = 100/256 = 35%)
+	LDI R17, 90
+	OUT OCR2, R17 ; R17 is also used in main loop for motor speed
 	
-	; Enable global interrupts
-	SEI
-	LDI R16, 0x00
+	; Enable interrupts
+	LDI R16, 0x11000000 ; Enable INT1 and INT0
 	OUT GICR, R16
-	LDI R16, 0b00000001 ; Set INT0 to trigger on any logical change
+	LDI R16, 0b00000101 ; Set INT1 and INT0 to trigger on any logical change
 	OUT MCUCR, R16
+	SEI ; Enable global interrupts
 	
 	; Set up ADC
 	;--------------------------------------------;
@@ -138,27 +129,27 @@ setup:
 	LDI R20, 0x20
 	LDI R21, 0x5
 	
+	
+	; BLUETOOTH
+	LDI R16, (1<<TXEN)|(1<<RXEN)
+	OUT UCSRB, R16
+	LDI R16, (1<<UCSZ1)|(1<<UCSZ0)|(1<<URSEL)
+	OUT UCSRC, R16
+	;LDI R16, 103		;16MHz Sætter baudrate til 9600, med U2X = 0 og error = 0,2%
+	LDI R16, 12		;1 MHz
+	OUT UBRRL, R16 
+	SBI UCSRA, U2X		;bruges til 1MHz baudrate
+	
 	RJMP main
 
 ;-------------------;
 ;     MAIN LOOP	    ;
 ;-------------------;
 main:
-	; Read FINISH_LINE sensor and if low (finish line detected), set RED_LED high
-	;SBIS FINISH_LINE_PIN, FINISH_LINE ; Skip next instruction if bit is clear (low)
-	;SBI RED_LED_PORT, RED_LED
+	; Set motor speed via bluetooth
+	RCALL Receive
+	OUT OCR2, R17
 	
-	;SBIC FINISH_LINE_PIN, FINISH_LINE ; Skip next instruction if bit is set (high)
-	;CBI RED_LED_PORT, RED_LED
-	
-	; Read DISTANCE sensor and if low (active), set GREEN_LED high
-	;SBIS DISTANCE_PIN, DISTANCE ; Skip next instruction if bit is clear (low)
-	;SBI GREEN_LED_PORT, GREEN_LED
-	
-	;SBIC DISTANCE_PIN, DISTANCE ; Skip next instruction if bit is clear (low)
-	;CBI GREEN_LED_PORT, GREEN_LED	
-	
-	MOTORSPEED R16, R20	
 	RJMP main
 	
 
@@ -167,37 +158,42 @@ main:
 ;-------------------;
 
 finish_line_interrupt:
-	ADD R20, R21
+	; Ignore interrupt if sensor is high (leaving the finish line)
+	SBIC FINISH_LINE_PIN, FINISH_LINE
+	RETI
+	
+	PUSH R16
+	
+	LDI R16, 0 ; Turn off motor
+	OUT OCR2, R16
+	
+	SBI RED_LED_PORT, RED_LED ; Turn on red LED
+	
+	RCALL delay_1sec
+	
+	LDI R16, 90 ; Turn motor back on
+	OUT OCR2, R16
+	
+	CBI RED_LED_PORT, RED_LED ; Turn off red LED
+	
+	POP R16	
+	
 	RETI
 
-
-;	SBIS RED_LED_PORT, RED_LED
-;	RJMP red_on
-;	RJMP red_off
-;
-;	red_on:
-;		SBI RED_LED_PORT, RED_LED
-;		RETI
-;		
-;	red_off:
-;		CBI RED_LED_PORT, RED_LED
-;		RETI
-
-;distance_interrupt:
-;	SBIS GREEN_LED_PORT, GREEN_LED
-;	RJMP green_on
-;	RJMP green_off
-;	
-;	green_on:
-;		SBI GREEN_LED_PORT, GREEN_LED
-;		RETI
-;		
-;	green_off:
-;		CBI RED_LED_PORT, RED_LED
-;		RETI	
+distance_interrupt:
+	PUSH R16
+	
+	; Toggle green LED
+	IN R16, GREEN_LED_PORT 
+	EORI R16, (1 << GREEN_LED)
+	OUT GREEN_LED_PORT, R16
+	
+	POP R16
+	
+	RETI
 
 ;-------------------;
-; DELAY SUB-ROUTINE ;
+;   SUB-ROUTINES 	;
 ;-------------------;
 delay_1sec:
 	LDI R23, 4 ;Load 61 into R23 (16 MHz = 61 loops, 1 MHz = 4 loops)
@@ -210,4 +206,11 @@ delay_1sec:
 	
 	DEC R23 ;Decrement R23
 	BRNE outer_loop ;Unless R23 overflows go back to outer_loop
-RET
+	RET
+
+
+Receive:
+		SBIS UCSRA, RXC
+		RET
+		IN	R17, UDR
+		RET
