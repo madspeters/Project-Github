3f3def4e9597b5056fd458af982148ca777ccaf9
Andreasgejlm <andreasgejlm@gmail.com>
2017-04-17 00:01:45 +0200

    strain gauge lavpasfilter

---
+++ Matlab-filer/lavpasfilter.m
@@ -0,0 +1,24 @@
+% Dimensionering af lavpasfilter til strain gauge.
+
+% Hvis en r?kke af de hurtigste sving l?gges lige efter hinanden, m? denne
+% frekvens v?re den teoretisk h?jeste som skal med.
+% 45 graders sving i inderbanen:
+pi = 3.14159;
+% Distance
+d = ;
+
+% Hastighed
+v = ;
+
+% Tiden det tager for bilen at k?re i svinget
+T = d/v;
+
+% frekvensen
+f = 1/T
+
+% Modstand r og kondensator C i lavpasfilter.
+r = ;
+C = ;
+f_0 = 1/(2*pi*r*C)
+
+
